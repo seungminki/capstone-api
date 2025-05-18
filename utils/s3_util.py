58938461858a1a9f2b3e8b04e793d7b3c19a76d2
Luji <fsjki0328@naver.com>
2025-05-18 17:31:14 +0900

    chore: 이미 모델이 있는 경우 다운로드 x

--- utils/s3_util.py
+++ utils/s3_util.py
@@ -6,15 +6,13 @@ from settings import AWS_S3_BUCKET_NAME, AWS_S3_KEY_ID, AWS_S3_SECRET_KEY
 
 
 def download_file_from_s3(local_dir, local_filename, s3_key):
-    # 전체 로컬 경로
+
     local_path = os.path.join(local_dir, local_filename)
 
-    # 파일이 이미 있으면 다운로드하지 않음
     if os.path.exists(local_path):
-        # print(f"'{filename}' already exists in '{local_dir}'")
+        print(f"already exists in '{local_dir}'")
         return
 
-    # S3 클라이언트 생성
     s3 = boto3.client(
         "s3",
         aws_access_key_id=AWS_S3_KEY_ID,
@@ -23,15 +21,18 @@ def download_file_from_s3(local_dir, local_filename, s3_key):
     )
 
     try:
-        # 다운로드
-        # print(f"Downloading '{filename}' from S3 bucket '{bucket_name}'...")
+        print(f"Downloading {s3_key} to {local_path} ...")
         s3.download_file(AWS_S3_BUCKET_NAME, s3_key, local_path)
-        # print("Download completed.")
+
     except Exception as e:
         print(f"Failed to download: {e}")
 
 
 def download_folder_from_s3(s3_prefix, local_dir):
+    if os.path.exists(local_dir):
+        print(f"already exists in '{local_dir}'")
+        return
+
     s3 = boto3.client(
         "s3",
         aws_access_key_id=AWS_S3_KEY_ID,
@@ -41,7 +42,6 @@ def download_folder_from_s3(s3_prefix, local_dir):
 
     response = s3.list_objects_v2(Bucket=AWS_S3_BUCKET_NAME, Prefix=s3_prefix)
 
-    # 'Contents' 키가 없을 수 있으므로 방어적 접근
     if "Contents" not in response:
         print(f"No files found in s3://{AWS_S3_BUCKET_NAME}/{s3_prefix}")
         return
@@ -51,7 +51,6 @@ def download_folder_from_s3(s3_prefix, local_dir):
     for obj in response["Contents"]:
         s3_key = obj["Key"]
 
-        # 디렉토리만 있는 경우 스킵
         if s3_key.endswith("/"):
             continue
 
@@ -60,7 +59,7 @@ def download_folder_from_s3(s3_prefix, local_dir):
         local_path = os.path.join(local_dir, relative_path)
         os.makedirs(os.path.dirname(local_path), exist_ok=True)
 
-        print(f"Downloading {s3_key} to {local_path}")
+        print(f"Downloading {s3_key} to {local_path} ...")
         s3.download_file(AWS_S3_BUCKET_NAME, s3_key, local_path)
 
     end = time.time()
