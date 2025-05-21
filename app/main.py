29a92a2c362ca894faa54f6451315949b70dadca
Luji <fsjki0328@naver.com>
2025-05-21 04:33:51 +0000

    chore: on event -> lifespan 변경 (on event 지양)

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, BackgroundTasks
 from app.routers import hatespeech
 from utils.s3_util import download_folder_from_s3
@@ -5,13 +6,41 @@ from utils.s3_util import download_folder_from_s3
 description = """
 CAPSTONE-2025 API helps you do awesome stuff. 🚀
 
-1. content-similarity (cs)
-2. hate-speech-detection (hd)
-3. topic-classification (tc)
+1. content-similarity: similarity
+2. hate-speech-detection: filter
+3. topic-classification: classification
 
 """
 
+
+def start():
+    def load_model():
+        download_folder_from_s3(
+            s3_prefix="hate-speech/250518/model",  # s3://my-ml-models/checkpoints/
+            local_dir="./hate-speech-model",  # 로컬 저장 위치
+        )
+
+    print("Service is starting....")
+    load_model()
+
+
+def shutdown():
+    print("Service is shutting down...")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # When service starts.
+    start()
+
+    yield
+
+    # When service is stopped.
+    shutdown()
+
+
 app = FastAPI(
+    lifespan=lifespan,
     title="Capstone-2025",
     description=description,
     summary="에브리타임 자동화 시스템 API",
@@ -19,17 +48,14 @@ app = FastAPI(
 )
 
 
-@app.on_event("startup")
-def load_model():
-    download_folder_from_s3(
-        s3_prefix="hate-speech/250518/model",  # s3://my-ml-models/checkpoints/
-        local_dir="./hate-speech-model",  # 로컬 저장 위치
-    )
-
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
 
 
 app.include_router(hatespeech.router)
